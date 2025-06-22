beb6c79cb1e4c7f30490a27cba0990da6c7c83d8
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2025-06-22 13:48:26 +0530

    Depth comments added

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,3 +1,4 @@
+//Basic comparisons
 // console.log(2 > 1)
 // console.log(2 >= 1)
 // console.log(2 < 1)
@@ -7,15 +8,17 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
+console.log(null > 0) //false
+console.log(null == 0)  //false
+console.log(null >= 0)  //true
 
-console.log(undefined == 0)
-console.log(undefined > 0)
-console.log(undefined < 0)
+console.log(undefined == 0) //false
+console.log(undefined > 0)  //false
+console.log(undefined < 0)  //false
 
-// === Compares the data type 
+// Avoid this type of conversion
+
+// === Strict Check -> Compares the data type also
 
 console.log("2" === 2);
 
