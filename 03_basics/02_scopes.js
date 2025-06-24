40fcc9e42b2135b239a58e6f8131af7438a2c3c8
Anurag Singh <anurocks.aps@gmail.com>
2025-06-24 15:52:20 +0530

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,13 +3,44 @@ let a = 300
 
 if (true) {
   let a = 20;
-  const b = 30;
-  
-  console.log("INNER:",a);
+  const b = 30;  
+  // console.log("INNER:",a);
   
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c); //only c will get printed..thats why we dont use var
+
+function one(){
+    const username = "anurag"
+
+    function two(){
+         const website = "youtube"
+         console.log(username);         
+    }
+    // console.log(website);
+
+    // two()
+    
+}
+
+// one()
+
+
+
+
+
+//++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++++
+
+// addone(5)
+function addone(num){
+  return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+   return num + 2
+}
 
