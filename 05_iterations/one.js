06ce79e6fcbd497cbfcb323b749f882bbb727311
Anurag Singh <anurocks.aps@gmail.com>
2025-06-24 20:42:39 +0530

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,18 @@
+//for
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     console.log(element);    
+    
+// }
+
+let myArray = ["flash", "peacemaker", "batman"]
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+    
+    
+}
+
+// break and continue
